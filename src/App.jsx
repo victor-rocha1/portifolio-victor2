7134752a343862301277a2f6302fae9c7c25a332
Victor Rocha <145049490+victor-rocha1@users.noreply.github.com>
2024-10-21 14:18:10 -0300

    Nova versão do meu portifólio

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css';
+import NavBar from './assets/sections/Header/NavBar';
+
+function App() {
+  return (
+    <NavBar />
+  );
+}
+
+export default App;
---
+++ src/assets/sections/Header/NavBar.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import logo from '../../../../public/images/logo.png';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './header.css';
+
+function NavBar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
+
+  return (
+    <header>
+      <nav className="navbar">
+        <a href="#">
+          <img src={logo} alt="logo" />
+        </a>
+        <div className="nav-links">
+          <a href="#">Home</a>
+          <a href="#">Sobre Mim</a>
+          <a href="#">Skills</a>
+          <a href="#">Experiências</a>
+        </div>
+        <button className='menu' onClick={toggleMenu}></button>
+      </nav>
+      <div className={`side-menu ${isMenuOpen ? 'open' : ''}`}>
+        <button className="close-menu" onClick={closeMenu}>X</button> {/* Botão para fechar */}
+        <a href="#">Home</a>
+        <a href="#">Sobre Mim</a>
+        <a href="#">Skills</a>
+        <a href="#">Experiências</a>
+      </div>
+    </header>
+  );
+}
+
+export default NavBar;
