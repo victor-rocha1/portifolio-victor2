5b300e731141f634af32ba5a67bbd1e9b07656b9
Victor Rocha <145049490+victor-rocha1@users.noreply.github.com>
2024-10-22 13:32:56 -0300

    Estilização do About

--- src/assets/sections/About/About.jsx
+++ src/assets/sections/About/About.jsx
@@ -10,7 +10,17 @@ function About() {
                     <div className="avatar">
                         <img src={avatar} alt="Avatar de Victor" />
                     </div>
-                    <Title text='Olá, sou Victor' spantext='Rocha' />
+                    {/* Exibindo o título e a lista separados */}
+                    <div className="about-content">
+                        <Title text='Victor' spantext='Rocha' />
+                        <p className="desc">Apaixonado por codificação</p>
+                        <ul className='description-list'>
+                            <li>Desenvolvedor Frontend</li>
+                            <li>Estudante do 4° período de Análise e Desenvolvimento de Sistemas</li>
+                            <li>Estudante de diversos cursos de tecnologia</li>
+                            <li>Estagiário na Prodemge</li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </section>
