9124d9ac498470f6f1b85bfa5fc4c6865aade672
Victor Rocha <145049490+victor-rocha1@users.noreply.github.com>
2024-10-28 12:16:27 -0300

    Estilização da Section Experiences

--- src/assets/sections/Experiences/Experiences.jsx
+++ src/assets/sections/Experiences/Experiences.jsx
@@ -1,21 +1,26 @@
+import Title from '../../components/Title/Title';
 import './experiences.css';
 
 function Experiences() {
     return (
-        <section className="experiências">
-            <div className="interface">
-                <div className="timeline">
-                    <div className="card">
-                        <img src="/images/logo_prodemge.png" alt="logo_prodemge" />
+        <>
+            <section className="experiências">
+                <br /><br /><Title text="MINHA " spantext="EXPERIÊNCIA" /> <br />
+                <div className="interface">
+                    <div className="timeline">
                         <div className="card-content">
-                            <small>2024 - <span>atual</span></small>
-                            <h5>Estagiário - <span>Prodemge</span></h5>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque ea debitis officia porro, amet nobis illo magni, corrupti provident deserunt facilis nam!</p>
+                            <img src="/images/logo_prodemge.png" alt="logo_prodemge" />
+                            <div className="text-content">
+                                <small>2024 - <span>atual</span></small>
+                                <h5>Estagiário - <span>Prodemge</span></h5>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque ea debitis officia porro, amet nobis illo magni, corrupti provident deserunt facilis nam!</p>
+                            </div>
                         </div>
+
                     </div>
                 </div>
-            </div>
-        </section>
+            </section>
+        </>
     );
 }
 
