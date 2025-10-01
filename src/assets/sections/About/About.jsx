80744fb444d47ea15c68c6a7af2e8d688a258141
Victor Rocha <145049490+victor-rocha1@users.noreply.github.com>
2025-09-30 21:31:54 -0300

    alteração de background-image

--- src/assets/sections/About/About.jsx
+++ src/assets/sections/About/About.jsx
@@ -1,23 +1,27 @@
 import './about.css';
 import avatarLight from '../../../../public/images/avatar.jpg';
 import avatarDark from '../../../../public/images/avatar-dark.jpg';
+import fundo from '../../../../public/images/fundo.png'; 
+import fundoEscuro from '../../../../public/images/fundo-escuro.png';
+
 import Title from '../../components/Title/Title';
 import Icons from '../../components/Icons/Icons';
 
-
 function About({ darkMode }) {
-
   const socialIcons = [
     { name: 'instagram', link: 'https://www.instagram.com/victor.rocha77/' },
     { name: 'github', link: 'https://www.github.com/victor-rocha1' },
   ];
 
   return (
-    <section className='about' id='about'>
+    <section
+      className='about'
+      id='about'
+      style={{ backgroundImage: `url(${darkMode ? fundoEscuro : fundo})` }}
+    >
       <div className="interface">
         <div className="about2">
           <div className="avatar">
-            {/* Troca a imagem com base na prop */}
             <img src={darkMode ? avatarDark : avatarLight} alt="Avatar de Victor" />
           </div>
           <div className="about-content">
@@ -25,7 +29,7 @@ function About({ darkMode }) {
             <p className="desc">Apaixonado por codificação</p>
             <ul className='description-list'>
               <li>Desenvolvedor Front End<span>.</span></li>
-              <li>Estudante do 5° período de Análise e Desenvolvimento de Sistemas<span>.</span></li>
+              <li>Estudante do 6° período de Análise e Desenvolvimento de Sistemas<span>.</span></li>
               <li>Estudante de diversos cursos na área de tecnologia, focados em Programação<span>.</span></li>
               <li>Estagiário na Prodemge<span>.</span></li>
             </ul>
